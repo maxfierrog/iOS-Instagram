a1f06cb16dbc53b938c816c70ebe0643b5958696
Max Fierro <maxfierro@berkeley.edu>
2022-06-27 11:47:30 -0700

    Added app icon and assets

--- instagram/AppDelegate.m
+++ instagram/AppDelegate.m
@@ -17,12 +17,22 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
-
             configuration.applicationId = @"jGpc7WjsJDFDI5IOzDM6su2ZJpEgnFlsS0dhW6dR";
             configuration.clientKey = @"57FZcKJWBVYq3YLvteFZVxIKHFG7SQNyK50BJskc";
             configuration.server = @"https://parseapi.back4app.com";
         }];
-        [Parse initializeWithConfiguration:config];
+    [Parse initializeWithConfiguration:config];
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+    gameScore[@"score"] = @1337;
+    gameScore[@"playerName"] = @"Sean Plott";
+    gameScore[@"cheatMode"] = @NO;
+    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            NSLog(@"Object saved!");
+        } else {
+            NSLog(@"Error: %@", error.description);
+        }
+    }];
     return YES;
 }
 
